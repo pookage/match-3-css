da9d6e662500fcef1bc9bd632a22ff5019d0d66c
pookage <pookagehayes@gmail.com>
2019-07-04 15:31:25 +0100

    minor styling tweaks

--- src/components/Grid/context.jsx
+++ src/components/Grid/context.jsx
@@ -3,7 +3,7 @@ import { reducer, generateGrid } from "./utils.js";
 import { ACTIONS } from "./";
 
 const Grid         = createContext();
-const width        = 10;
+const width        = 15;
 const height       = 15;
 const colors      = ["#2442d6", "#7fdb42", "#bc2bd6", "#f5ef42", "#c72020", "#10e3df"];
 const grid         = generateGrid(width, height, colors);
